b7c488e69c49f146c0ec7cbe1d9ef13db1ce3056
Vinicius da Silva Lourenco <vinicius.vsl@gmail.com>
2020-07-16 16:23:35 -0300

    Deleting products using async requests

--- controllers/admin.js
+++ controllers/admin.js
@@ -146,8 +146,8 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
-exports.postDeleteProduct = (req, res, next) => {
-  const prodId = req.body.productId;
+exports.deleteProduct = (req, res, next) => {
+  const prodId = req.params.productId;
   Product.findById(prodId)
     .then((product) => {
       if (!product) {
@@ -157,11 +157,9 @@ exports.postDeleteProduct = (req, res, next) => {
       return Product.deleteOne({ _id: prodId, userId: req.session.user });
     })
     .then(() => {
-      res.redirect('/admin/products');
+      res.status(200).json({message: "Success"});
     })
     .catch((err) => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      res.status(500).json({message: "Deleting product failed."});
     });
 };
